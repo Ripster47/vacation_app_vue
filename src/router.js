08c3a5bf81b41b628b71f4076ef55425fc8409f7
Ripster47 <mpolinski12@gmail.com>
2019-02-27 15:42:52 -0600

    User successfully created from font

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import ListIndex from './views/ListIndex.vue'
+import SignUp from './views/SignUp.vue'
+import Login from './views/Login.vue'
 
 Vue.use(Router)
 
@@ -8,18 +10,10 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    { path: '/', name: 'sign-up', component: SignUp },
+    { path: '/lists', name: 'lists-index', component: ListIndex },
+    { path: '/login', name: 'login', component: Login }
+
+
   ]
 })
---
+++ src/views/ListIndex.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="lists-index">
+
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+var axios = require('axios');
+
+export default {
+  data: function() {
+    return {
+
+    };
+  },
+  created: function() {},
+  methods: {}
+};
+</script>
