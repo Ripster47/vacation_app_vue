aa8ae825d493eb792646accf21243d2ee6ce6e89
Leo Gonzalez <lgonz053@gmail.com>
2019-02-28 16:41:28 -0600

    show is working. need to add more

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import ListIndex from './views/ListIndex.vue'
 import SignUp from './views/SignUp.vue'
 import Login from './views/Login.vue'
+import ListShow from './views/ListShow.vue'
 
 Vue.use(Router)
 
@@ -11,9 +12,8 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     { path: '/', name: 'sign-up', component: SignUp },
+    { path: '/login', name: 'login', component: Login },
     { path: '/lists', name: 'lists-index', component: ListIndex },
-    { path: '/login', name: 'login', component: Login }
-
-
+    { path: '/lists/:id', name: 'lists-show', component: ListShow }
   ]
 })
