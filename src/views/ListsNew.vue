50e60fad6cec7ff92d4eedce28f5c1f34575f807
anjelicakay <aktowner@gmail.com>
2019-02-28 19:10:21 -0600

    created list new and edit

---
+++ src/views/ListsNew.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="lists-new">
+    <ul>
+      <li v-for="error in errors"> {{ error }} </li>
+    </ul>
+    <div class='container'>
+      <h2>Create a checklist!</h2>
+      <form v-on:submit.prevent="submit()">
+        <div class="form-group">
+          <label>Destination: </label>
+          <input class="form-control" type="text" v-model="list.name" placeholder="Destination">
+        </div>
+        <div class="form-group">
+          <label>Date of Trip: </label>
+          <input class="form-control" type="text" v-model="list.date" placeholder="DD/MM/YYYY">
+        </div>
+        <input type="submit" value="Create" class="btn btn-info">
+      </form>
+    </div>
+  </div>
+</template>
+
+
+<style>
+.container{
+  margin-bottom: 30px;
+}
+</style>
+
+
+<script>
+var axios = require('axios');
+
+export default {
+  data: function() {
+    return {
+      list: {
+              name: "",
+              date: "",
+              user_id: ""
+            },
+      errors: []  
+    };      
+  },
+  created: function() {},
+  methods: {
+    sumbit: function() {
+      var params = {
+                    name: this.list.name,
+                    date: this.list.date,
+                    user_id: this.list.user_id
+                    };
+      axios.post("/api/lists", params)
+        .then(response => {
+          this.$router.push("/lists/" + response.data.id);
+        }).catch(error => {
+          this.errors = error.response.data.errors;
+        });              
+    } 
+  }    
+}
+</script>
